511cb850b64ac8987c9a6f5060399ad7298d8c8a
Yeongjae Kim <sees77770@gmail.com>
2024-11-08 20:26:32 +0900

    refactor: Product 테스트 BeforeEach 추가
    반복되는 Product 초기화를 BeforeEach로 묶는다.

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -4,21 +4,26 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
+    private Product product;
 
-    @DisplayName("상품은 이름, 가격, 재고, 프로모션을 가진다.")
-    @Test
-    void productTest() {
+    @BeforeEach
+    void setUp() {
         String name = "콜라";
         int price = 1000;
         int quantity = 10;
         String promotion = "탄산2+1";
 
-        Product product = new Product(name, price, quantity, promotion);
+        product = new Product(name, price, quantity, promotion);
+    }
 
+    @DisplayName("상품은 이름, 가격, 재고, 프로모션을 가진다.")
+    @Test
+    void productTest() {
         assertAll("product",
                 () -> assertEquals("콜라", product.name),
                 () -> assertEquals(1000, product.price),
@@ -30,13 +35,6 @@ class ProductTest {
     @DisplayName("상품명이 같은지 확인한다.")
     @Test
     void equalNameTest() {
-        String name = "콜라";
-        int price = 1000;
-        int quantity = 10;
-        String promotion = "탄산2+1";
-
-        Product product = new Product(name, price, quantity, promotion);
-
         boolean isSameName = product.isSameName("콜라");
 
         assertThat(isSameName).isTrue();
