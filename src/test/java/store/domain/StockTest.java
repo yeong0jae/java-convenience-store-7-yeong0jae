31c7ce23a27a0350ff9e7f53cef0bf637e921ff9
Yeongjae Kim <sees77770@gmail.com>
2024-11-08 19:59:58 +0900

    refactor: StockTest에 BeforeEach 추가
    반복되는 given인 products 데이터를 BeforeEach로 묶는다.

--- src/test/java/store/domain/StockTest.java
+++ src/test/java/store/domain/StockTest.java
@@ -3,19 +3,24 @@ package store.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StockTest {
+    private List<Product> products;
+
+    @BeforeEach
+    void setUp() {
+        products = List.of(
+                new Product("콜라", 1000, 10, "탄산2+1"),
+                new Product("콜라", 1000, 10, null)
+        );
+    }
 
     @DisplayName("상품 목록을 가진다.")
     @Test
     void stockTest() {
-        List<Product> products = List.of(
-                new Product("콜라", 1000, 10, "탄산2+1"),
-                new Product("콜라", 1000, 10, null)
-        );
-
         Stock stock = new Stock(products);
 
         assertThat(stock.products).isEqualTo(products);
@@ -24,14 +29,16 @@ class StockTest {
     @DisplayName("구매자가 가져온 상품명으로 상품이 존재하는지 확인한다.")
     @Test
     void hasProductTest() {
-        List<Product> products = List.of(
-                new Product("콜라", 1000, 10, "탄산2+1"),
-                new Product("콜라", 1000, 10, null)
-        );
         Stock stock = new Stock(products);
 
         boolean hasProduct = stock.hasProduct("콜라");
 
         assertThat(hasProduct).isTrue();
+    }
+
+    @DisplayName("구매자가 가져온 상품이 존재하지 않으면 예외 처리한다.")
+    @Test
+    void hasProductExceptionTest() {
+
     }
 }
