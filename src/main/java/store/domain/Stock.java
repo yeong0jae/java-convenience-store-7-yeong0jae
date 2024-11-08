3d9954e9804cc889888d3295cc5db4320c75d2cd
Yeongjae Kim <sees77770@gmail.com>
2024-11-08 21:58:57 +0900

    style: 클래스 내 메서드 순서 수정

--- src/main/java/store/domain/Stock.java
+++ src/main/java/store/domain/Stock.java
@@ -19,18 +19,18 @@ public class Stock {
         return true;
     }
 
+    public boolean hasEnoughStock(String name, int quantity) {
+        if (quantity > findQuantityByName(name)) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
+        return true;
+    }
+
     private int findQuantityByName(String name) {
         return products.stream().filter(
                 product -> product.isSameName(name)
         ).mapToInt(
                 Product::getQuantity
         ).sum();
-    }
-
-    public boolean hasEnoughStock(String name, int quantity) {
-        if (quantity > findQuantityByName(name)) {
-            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
-        }
-        return true;
     }
 }
