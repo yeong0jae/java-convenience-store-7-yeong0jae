a48a5ef1b617dd0146096043c4e4c603f894311f
Yeongjae Kim <sees77770@gmail.com>
2024-11-09 12:28:31 +0900

    feat: OrderItem 클래스 추가
    구매자의 구매 요청 기능을 위해 개별 주문 상품을 가르키는
    OrderItem 클래스를 추가한다.

---
+++ src/main/java/store/domain/OrderItem.java
@@ -0,0 +1,11 @@
+package store.domain;
+
+public class OrderItem {
+    String name;
+    int quantity;
+
+    public OrderItem(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
---
+++ src/test/java/store/domain/OrderItemTest.java
@@ -0,0 +1,21 @@
+package store.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderItemTest {
+
+    @DisplayName("구매는 상품명, 수량을 가진다.")
+    @Test
+    void orderItemTest() {
+        String name = "콜라";
+        int quantity = 10;
+
+        OrderItem orderItem = new OrderItem(name, quantity);
+
+        assertThat(orderItem.name).isEqualTo(name);
+        assertThat(orderItem.quantity).isEqualTo(quantity);
+    }
+}
