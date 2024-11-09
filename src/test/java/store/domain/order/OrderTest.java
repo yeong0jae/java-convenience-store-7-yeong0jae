5a8d35c7027105cdb88d743c4af8e3dc4de6cce9
Yeongjae Kim <sees77770@gmail.com>
2024-11-09 16:51:48 +0900

    refactor: OrderTest에 BeforeEach를 추가

--- src/test/java/store/domain/order/OrderTest.java
+++ src/test/java/store/domain/order/OrderTest.java
@@ -3,20 +3,25 @@ package store.domain.order;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
+    private Order order;
 
-    @DisplayName("주문 상품 이름 목록을 구한다.")
-    @Test
-    void getOrderItemNamesTest() {
+    @BeforeEach
+    void setUp() {
         List<OrderItem> orderItems = List.of(
                 new OrderItem("콜라", 10),
                 new OrderItem("탄산수", 3)
         );
-        Order order = new Order(orderItems);
+        order = new Order(orderItems);
+    }
 
+    @DisplayName("주문 상품 이름 목록을 구한다.")
+    @Test
+    void getOrderItemNamesTest() {
         List<String> orderItemNames = order.getOrderItemNames();
 
         assertThat(orderItemNames).isEqualTo(List.of("콜라", "탄산수"));
@@ -25,12 +30,6 @@ class OrderTest {
     @DisplayName("주문 수량을 확인한다.")
     @Test
     void findQuantityByNameTest() {
-        List<OrderItem> orderItems = List.of(
-                new OrderItem("콜라", 10),
-                new OrderItem("탄산수", 3)
-        );
-        Order order = new Order(orderItems);
-
         int orderQuantity = order.findQuantityByName("콜라");
 
         assertThat(orderQuantity).isEqualTo(10);
