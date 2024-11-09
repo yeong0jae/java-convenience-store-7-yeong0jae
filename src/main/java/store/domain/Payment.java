ddca03fa34aecb31cf52145ded51af169c98cafe
Yeongjae Kim <sees77770@gmail.com>
2024-11-09 14:29:05 +0900

    refactor: 개별 상품 구매액, 총구매액 계산 로직 분리

--- src/main/java/store/domain/Payment.java
+++ src/main/java/store/domain/Payment.java
@@ -1,13 +1,15 @@
 package store.domain;
 
 public class Payment {
-
+    
     public int calculateTotalPurchaseAmount(Order order, Stock stock) {
         return order.orderItems.stream()
-                .mapToInt(orderItem -> {
-                    int price = stock.findPriceByName(orderItem.name);
-                    return orderItem.quantity * price;
-                })
+                .mapToInt(item -> calculatePurchaseAmount(item, stock))
                 .sum();
+    }
+
+    private int calculatePurchaseAmount(OrderItem orderItem, Stock stock) {
+        int price = stock.findPriceByName(orderItem.name);
+        return orderItem.quantity * price;
     }
 }
