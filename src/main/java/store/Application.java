9830f7bc5892eade5025081ee5820d656cd60677
Yeongjae Kim <sees77770@gmail.com>
2024-11-10 12:02:43 +0900

    feat: 편의점 콘솔 view 생성
    - Input을 담당하는 InputView 생성
    - Output을 담당하는 OutputView 생성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,8 +1,14 @@
 package store;
 
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        ConvenienceStore convenienceStore = new ConvenienceStore();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        ConvenienceStore convenienceStore = new ConvenienceStore(inputView, outputView);
         convenienceStore.open();
     }
 }
--- src/main/java/store/ConvenienceStore.java
+++ src/main/java/store/ConvenienceStore.java
@@ -1,6 +1,17 @@
 package store;
 
+import store.view.InputView;
+import store.view.OutputView;
+
 public class ConvenienceStore {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ConvenienceStore(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 
     public void open() {
     }
