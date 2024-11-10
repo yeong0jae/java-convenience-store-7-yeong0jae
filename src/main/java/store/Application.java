fadd860a9b5030c00101b5b64c9ed2f2d9899a7e
Yeongjae Kim <sees77770@gmail.com>
2024-11-10 12:01:12 +0900

    feat: 편의점 컨트롤러 생성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,6 +2,7 @@ package store;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConvenienceStore convenienceStore = new ConvenienceStore();
+        convenienceStore.open();
     }
 }
