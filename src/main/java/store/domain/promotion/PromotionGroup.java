a971234f3dfdefa19e8aaad1170abdaba5a52b12
Yeongjae Kim <sees77770@gmail.com>
2024-11-10 10:01:00 +0900

    refactor: 프로모션 타입 삭제
    promotion.md에서 프로모션 정보를 받기 위해, 프로모션 타입은 삭제한다.

--- src/main/java/store/domain/promotion/PromotionGroup.java
+++ src/main/java/store/domain/promotion/PromotionGroup.java
@@ -1,19 +1,15 @@
 package store.domain.promotion;
 
-import java.util.List;
-
 public enum PromotionGroup {
-    TWO_PLUS_ONE(2, 1, List.of(PromotionType.CARBONATED_TWO_PLUS_ONE)),
-    ONE_PLUS_ONE(1, 1, List.of(PromotionType.MD_RECOMMENDED, PromotionType.SHINY_DISCOUNT));
+    TWO_PLUS_ONE(2, 1),
+    ONE_PLUS_ONE(1, 1);
 
     public final int buy;
     public final int get;
-    private final List<PromotionType> promotionTypes;
 
-    PromotionGroup(int buy, int get, List<PromotionType> promotionTypes) {
+    PromotionGroup(int buy, int get) {
         this.buy = buy;
         this.get = get;
-        this.promotionTypes = promotionTypes;
     }
 
     public static PromotionGroup fromBuyGet(int buy, int get) {
