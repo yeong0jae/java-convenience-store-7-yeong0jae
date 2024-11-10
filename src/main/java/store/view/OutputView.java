6d4893d59821d53c49a0527b09c7d0564e75b88c
Yeongjae Kim <sees77770@gmail.com>
2024-11-11 08:14:34 +0900

    refactor: 영수증 출력 메서드 분리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -24,22 +24,35 @@ public class OutputView {
 
     public void printReceipt(Receipt receipt) {
         System.out.println("===========W 편의점=============");
+        printPurchaseHistories(receipt);
+        printGivenProducts(receipt);
+        printDiscounts(receipt);
+    }
+
+    private void printPurchaseHistories(Receipt receipt) {
         System.out.println("상품명\t\t수량\t금액");
-        receipt.getPurchaseHistories().forEach(purchaseHistory -> {
-            System.out.println(purchaseHistory.getName() + "\t\t" + purchaseHistory.getCount() + " \t"
-                    + numberFormat(purchaseHistory.getAmount()));
-        });
+        receipt.getPurchaseHistories().forEach(purchaseHistory ->
+                System.out.println(purchaseHistory.getName() + "\t\t" + purchaseHistory.getCount() + " \t"
+                        + numberFormat(purchaseHistory.getAmount()))
+        );
+    }
+
+    private void printGivenProducts(Receipt receipt) {
         System.out.println("===========증\t정==============");
-        receipt.getGivenProducts().forEach(givenProduct -> {
-            System.out.println(givenProduct.getName() + "\t\t" + givenProduct.getCount());
-        });
+        receipt.getGivenProducts().forEach(givenProduct ->
+                System.out.println(givenProduct.getName() + "\t\t" + givenProduct.getCount())
+        );
         System.out.println("==============================");
+    }
+
+    private void printDiscounts(Receipt receipt) {
         System.out.println(
                 "총구매액\t\t" + receipt.getPurchaseCount() + "\t" + numberFormat(receipt.getTotalPurchaseAmount()));
         System.out.println("행사할인\t\t\t-" + numberFormat(receipt.getPromotionDiscount()));
         System.out.println("멤버십할인\t\t\t-" + numberFormat(receipt.getMembershipDiscount()));
         System.out.println("내실돈\t\t\t " + numberFormat(receipt.getPaymentAmount()));
     }
+
 
     private String numberFormat(int number) {
         return NumberFormat.getInstance().format(number);
