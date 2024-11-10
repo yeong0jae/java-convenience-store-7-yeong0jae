88b7145b82f44ebcc13e87a4d7a011e11b9a83d8
Yeongjae Kim <sees77770@gmail.com>
2024-11-10 23:39:33 +0900

    feat: 예, 아니오 입력 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -19,4 +19,23 @@ public class InputView {
         String replacedInput = rawOrderItem.replace("[", "").replace("]", "");
         return Arrays.asList(replacedInput.split("-"));
     }
+
+    public boolean readAdditionalPromotion(String name, int count) {
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", name, count);
+        return isYes();
+    }
+
+    public boolean readNoApplicablePromotion(String name, int count) {
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n", name, count);
+        return isYes();
+    }
+
+    public boolean readMembershipDiscount() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return isYes();
+    }
+
+    private boolean isYes() {
+        return Console.readLine().equalsIgnoreCase("Y");
+    }
 }
