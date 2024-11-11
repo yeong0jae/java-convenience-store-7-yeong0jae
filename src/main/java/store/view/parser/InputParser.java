3a5cd45ebaf704cc96b663e7a1dc158ebd250b32
Yeongjae Kim <sees77770@gmail.com>
2024-11-11 16:38:15 +0900

    refactor: 입력 검증 메서드 분리

--- src/main/java/store/view/parser/InputParser.java
+++ src/main/java/store/view/parser/InputParser.java
@@ -6,15 +6,42 @@ import java.util.List;
 public class InputParser {
 
     public List<String> parseOrderItem(String rawOrderItem) {
+        validateBrackets(rawOrderItem);
+
         String replacedInput = rawOrderItem.replace("[", "").replace("]", "");
         List<String> parts = Arrays.asList(replacedInput.split("-"));
 
-        if (parts.size() != 2 || parts.get(0).isEmpty() || !isNumeric(parts.get(1))) {
-            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
-        }
+        validatePartsSize(parts);
+        validateProductName(parts);
+        validateQuantity(parts);
 
         return parts;
     }
+
+    private void validateBrackets(String rawOrderItem) {
+        if (!rawOrderItem.startsWith("[") || !rawOrderItem.endsWith("]")) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validatePartsSize(List<String> parts) {
+        if (parts.size() != 2) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateProductName(List<String> parts) {
+        if (parts.get(0).isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateQuantity(List<String> parts) {
+        if (!isNumeric(parts.get(1))) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
 
     private boolean isNumeric(String str) {
         return str.chars().allMatch(Character::isDigit);
