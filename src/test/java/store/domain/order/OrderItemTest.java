248759611ee8d0216e88afcbcb142b1321b1af8d
Yeongjae Kim <sees77770@gmail.com>
2024-11-11 12:09:29 +0900

    test: get 만큼 주문 수량 추가 테스트

--- src/test/java/store/domain/order/OrderItemTest.java
+++ src/test/java/store/domain/order/OrderItemTest.java
@@ -1,8 +1,10 @@
 package store.domain.order;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import store.util.ErrorMessage;
@@ -18,5 +20,17 @@ class OrderItemTest {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> new OrderItem(name, count)
         ).withMessage(ErrorMessage.PREFIX + "1개 이상의 상품만 구매할 수 있습니다.");
+    }
+
+    @DisplayName("get 만큼 주문 수량을 추가한다.")
+    @Test
+    void addCountTest() {
+        String name = "콜라";
+        String count = "1";
+        OrderItem orderItem = new OrderItem(name, count);
+
+        orderItem.addCount(2);
+
+        assertThat(orderItem.getCount()).isEqualTo(3);
     }
 }
